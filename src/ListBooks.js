d19f70bc2d8176d83bda2c980f347f618b110c63
valtermartins1301 <valtermartins.1301@gmail.com>
2017-12-20 00:42:53 -0200

    use PropTypes to verify required params

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types';
 
 class ListBooks extends Component {
+  static propTypes = {
+    books: PropTypes.array.isRequired
+  }
+
   render() {
 
     return (
