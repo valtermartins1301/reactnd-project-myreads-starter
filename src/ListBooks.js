98bc214a1a6b5b3fcbf511d3425ff173983c1234
valtermartins1301 <valtermartins.1301@gmail.com>
2017-12-20 00:43:14 -0200

    list fetched books

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types';
+import Bookshelf from './Bookshelf';
 
 class ListBooks extends Component {
   static propTypes = {
@@ -8,6 +9,10 @@ class ListBooks extends Component {
   }
 
   render() {
+    const { books } = this.props;
+    const currentlyReading = books.filter((book) => book.shelf === 'currentlyReading');
+    const wantToRead = books.filter((book) => book.shelf === 'wantToRead');
+    const read = books.filter((book) => book.shelf === 'read');
 
     return (
       <div className="list-books">
@@ -15,6 +20,9 @@ class ListBooks extends Component {
           <h1>MyReads</h1>
         </div>
         <div className="list-books-content">
+          <Bookshelf books={currentlyReading} shelfTitle='Currently Reading'/>
+          <Bookshelf books={wantToRead} shelfTitle='Want To Read'/>
+          <Bookshelf books={read} shelfTitle='Read'/>
         </div>
         <div className="open-search">
           <Link to='/search'>Add a book</Link>
