d97816037e53f93d689519e071191aeaf62d22ff
valtermartins1301 <valtermartins.1301@gmail.com>
2018-01-04 13:26:41 -0200

    search books no results message

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,42 +1,57 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
-import React, { Component } from 'react';
-import {DebounceInput} from 'react-debounce-input';
-import PropTypes from 'prop-types';
-import Book from './Book';
+import { DebounceInput } from 'react-debounce-input';
+import BooksGrid from './BooksGrid';
 
-class SearchBooks extends Component {
-  static propTypes = {
-    books: PropTypes.array.isRequired,
-    onSearch: PropTypes.func.isRequired,
-  }
-
-  render() {
-    const { onSearch, books } = this.props;
-
-    return (
-      <div className="search-books">
-        <div className="search-books-bar">
-          <Link className="close-search" to='/'>Close</Link>
-          <div className="search-books-input-wrapper">
-            <DebounceInput
-              minLength={2}
-              debounceTimeout={300}
-              placeholder="Search by title or author"
-              onChange={event => onSearch(event.target.value)} />
-          </div>
-        </div>
-        <div className="search-books-results">
-          <ol className="books-grid">
-            {books.map((book) => (
-              <li key={book.id}>
-                <Book book={book}/>
-              </li>
-            ))}
-          </ol>
-        </div>
-      </div>
-    )
-  }
+function NotFound() {
+  return (
+    <p className="search-books-no-results">
+      Nenhum livro encontrado...
+    </p>
+  );
 }
 
-export default SearchBooks
+function SearchResult({ books, changeShelf }) {
+  if (books.length > 0) {
+    return (
+      <BooksGrid
+        books={books}
+        changeShelf={changeShelf}
+      />
+    );
+  }
+
+  return <NotFound />;
+}
+
+function SearchBooks({ books, onSearch, onUpdate }) {
+  return (
+    <div className="search-books">
+      <div className="search-books-bar">
+        <Link className="close-search" to="/">Close</Link>
+        <div className="search-books-input-wrapper">
+          <DebounceInput
+            minLength={2}
+            debounceTimeout={300}
+            placeholder="Search by title or author"
+            onChange={event => onSearch(event.target.value)}
+          />
+        </div>
+      </div>
+      <div className="search-books-results">
+        <SearchResult
+          books={books}
+          changeShelf={onUpdate}
+        />
+      </div>
+    </div>
+  );
+}
+
+SearchBooks.defaultProps = {
+  books: [],
+  onSearch: () => {},
+  onUpdate: () => {},
+};
+
+export default SearchBooks;
