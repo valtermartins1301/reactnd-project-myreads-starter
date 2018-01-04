ecf9c6d3e81e70c0aa8cf6b57795bf639fbd992e
valtermartins1301 <valtermartins.1301@gmail.com>
2018-01-04 13:30:17 -0200

    change component to function

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,33 +1,21 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import Book from './Book';
+import React from 'react';
+import BooksGrid from './BooksGrid';
 
-class Bookshelf extends Component {
-  static propTypes = {
-    books: PropTypes.array.isRequired,
-    shelfTitle: PropTypes.string.isRequired,
-  }
-
-  render() {
-    const { books, shelfTitle } = this.props;
-
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{shelfTitle}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {books.map((book) => (
-              <li key={book.id}>
-                <Book
-                  book={book}
-                />
-              </li>
-            ))}
-          </ol>
-        </div>
+function Bookshelf({ books, shelfTitle, changeShelf }) {
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{shelfTitle}</h2>
+      <div className="bookshelf-books">
+        <BooksGrid books={books} changeShelf={changeShelf} />
       </div>
-    )
-  }
+    </div>
+  );
 }
+
+Bookshelf.defaultProps = {
+  books: [],
+  shelfTitle: '',
+  changeShelf: () => {},
+};
 
 export default Bookshelf;
