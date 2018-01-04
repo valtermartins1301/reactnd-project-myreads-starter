00297ee68ee590941fba664f3bb749c2433092e9
valtermartins1301 <valtermartins.1301@gmail.com>
2018-01-04 18:10:30 -0200

    create progress component

---
+++ src/components/Progress.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Progress() {
+  return (
+    <div className="progress">
+      <div className="indeterminate" />
+    </div>
+  );
+}
+
+export default Progress;
