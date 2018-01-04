36f8b5fa7769354681f9b91e8ae6f036dbb36eb2
valtermartins1301 <valtermartins.1301@gmail.com>
2018-01-04 13:30:04 -0200

    change component to function

--- src/Book.js
+++ src/Book.js
@@ -1,33 +1,25 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
+import SelfChanger from './SelfChanger';
 
-class Book extends Component {
-  static propTypes = {
-    book: PropTypes.object.isRequired
-  }
-
-  render() {
-    const { book } = this.props;
-
-    return (
-      <div className="book">
-        <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-          <div className="book-shelf-changer">
-            <select>
-              <option value="none" disabled>Move to...</option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
-          </div>
+function Book({ book, changeShelf }) {
+  return (
+    <div className="book">
+      <div className="book-top">
+        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }} />
+        <div className="book-shelf-changer">
+          <SelfChanger book={book} changeShelf={changeShelf} />
         </div>
-        <div className="book-title">{book.title}</div>
-        <div className="book-authors">{book.authors.toString()}</div>
       </div>
-    )
-  }
+      <div className="book-title">{book.title}</div>
+      <div className="book-authors">{() => book.authors.toString()}</div>
+    </div>
+  );
 }
+
+Book.defaultProps = {
+  book: {},
+  changeShelf: () => {},
+};
+
 
 export default Book;
