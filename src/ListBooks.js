2cd6e8d3a1a8418ff36acdc393f4a26aa8479ae2
valtermartins1301 <valtermartins.1301@gmail.com>
2018-01-04 13:30:31 -0200

    change component to function

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,35 +1,44 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
-import PropTypes from 'prop-types';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import Bookshelf from './Bookshelf';
 
-class ListBooks extends Component {
-  static propTypes = {
-    books: PropTypes.array.isRequired
-  }
-
-  render() {
-    const { books } = this.props;
-    const currentlyReading = books.filter((book) => book.shelf === 'currentlyReading');
-    const wantToRead = books.filter((book) => book.shelf === 'wantToRead');
-    const read = books.filter((book) => book.shelf === 'read');
-
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          <Bookshelf books={currentlyReading} shelfTitle='Currently Reading'/>
-          <Bookshelf books={wantToRead} shelfTitle='Want To Read'/>
-          <Bookshelf books={read} shelfTitle='Read'/>
-        </div>
-        <div className="open-search">
-          <Link to='/search'>Add a book</Link>
-        </div>
+function ListBooks({
+  currentlyReading, wantToRead, read, onUpdate,
+}) {
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
       </div>
-    )
-  }
+      <div className="list-books-content">
+        <Bookshelf
+          books={currentlyReading}
+          shelfTitle="Currently Reading"
+          changeShelf={onUpdate}
+        />
+        <Bookshelf
+          books={wantToRead}
+          shelfTitle="Want To Read"
+          changeShelf={onUpdate}
+        />
+        <Bookshelf
+          books={read}
+          shelfTitle="Read"
+          changeShelf={onUpdate}
+        />
+      </div>
+      <div className="open-search">
+        <Link to="/search">Add a book</Link>
+      </div>
+    </div>
+  );
 }
 
-export default ListBooks
+ListBooks.defaultProps = {
+  currentlyReading: [],
+  wantToRead: [],
+  read: [],
+  onUpdate: () => {},
+};
+
+export default ListBooks;
