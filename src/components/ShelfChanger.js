317761e071766b5489dcab98273f979e6361f247
valtermartins1301 <valtermartins.1301@gmail.com>
2018-01-08 08:05:22 -0200

    fix searchedBooks state

--- src/components/ShelfChanger.js
+++ src/components/ShelfChanger.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class ShelfChanger extends Component {
   state = {
-    shelf: 'none',
+    shelf: this.props.book.shelf || 'none',
   }
 
   handleChange = ({ target }) => {
@@ -17,7 +17,7 @@ class ShelfChanger extends Component {
     return (
       <div className="book-shelf-changer">
         <select value={this.state.shelf} onChange={this.handleChange}>
-          <option value="none" disabled>Move to...</option>
+          <option default disabled>Move to...</option>
           <option value="currentlyReading">Currently Reading</option>
           <option value="wantToRead">Want to Read</option>
           <option value="read">Read</option>
