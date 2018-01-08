c29bc5af00e4ba9526bb8489e56f7961c6beefcb
valtermartins1301 <valtermartins.1301@gmail.com>
2018-01-08 08:04:53 -0200

    fix undefined author and background images

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ShelfChanger from './ShelfChanger';
 
-function getAuthors(book) {
-  return book.authors.toString();
+function getAuthors({ authors = [] }) {
+  return authors.toString();
+}
+function getThumbnail({ imageLinks = {} }) {
+  return imageLinks.thumbnail;
 }
 
 function Book({ book, changeShelf }) {
   return (
     <div className="book">
       <div className="book-top">
-        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }} />
+        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${getThumbnail(book)})` }} />
         <ShelfChanger book={book} changeShelf={changeShelf} />
       </div>
       <div className="book-title">{book.title}</div>
